2b16e65ed057ad612726f438226671ab270e7f8e
Luke Yost <lukexyost>
2022-05-25 14:20:03 -0600

    made files and house model

---
+++ app/Models/HouseModel.js
@@ -0,0 +1,9 @@
+export class House{
+  constructor(name, roomNumber, bedrooms, bathrooms, sqrFootage){
+    this.name = name 
+    this.roomNumber = roomNumber
+    this.bedrooms = bedrooms 
+    this.bathrooms = bathrooms 
+    this.sqrFootage = sqrFootage 
+  }
+}
